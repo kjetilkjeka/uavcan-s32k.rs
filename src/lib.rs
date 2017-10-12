8d40ceaad068f1cf30d8abe6e06f9535826f2100
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-10-12 12:07:51 +0200

    Added shell for implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,37 @@
+#![feature(alloc)]
 #![no_std]
 
-#[macro_use]
-extern crate cortex_m;
-#[macro_use]
+extern crate uavcan;
+extern crate s32k144;
 extern crate s32k144evb;
+extern crate embedded_types;
+extern crate alloc;
 
-use cortex_m::asm;
+use uavcan::transfer::TransferFrame;
+use uavcan::transfer::TransferInterface;
+use uavcan::transfer::FullTransferID;
+use uavcan::transfer::IOError;
 
-use s32k144evb::wdog;
+use alloc::Vec;
+
+use embedded_types::can::ExtendedDataFrame as CanFrame;
+
+pub struct Interface<'a> {
+    interface: &'a s32k144::can0::RegisterBlock,
+}
+
+impl<'a> Interface<'a> {
+    pub fn new(can: &'a s32k144::can0::RegisterBlock) -> Self {
+        Interface{interface: can}
+    }
+}
+
+impl<'a> TransferInterface<'a> for Interface<'a> {
+    type Frame = CanFrame;
+    type IDContainer = &'a [FullTransferID];
+
+    fn transmit(&self, frame: &CanFrame) -> Result<(), IOError> {unimplemented!()}
+    fn receive(&self, identifier: &FullTransferID) -> Option<CanFrame> {unimplemented!()}
+    fn completed_receives(&self, identifier: FullTransferID, mask: FullTransferID) -> Self::IDContainer {unimplemented!()}    
+}
 
